4ca0a21b1b0a0b7d8ec839ff5347e6279ad95b16
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2023-10-29 17:24:55 +0000

    1 feito

--- ex2.py
+++ ex2.py
@@ -3,17 +3,33 @@ import math
 
 #Exercício 1
 
+
 #definir valores iniciais de x e y:
 x = -2 
 y = -3  
 #definir passo
 x_p = 0.1 
 y_p = 0.2
-#definir "range"
+#definir "range" (i_r linhas e j_r colunas)
 i_r = (abs(x)*2)/x_p    #assumindo que o intervalo é sempre [-a,a]
 j_r = (abs(y)*2)/y_p    #   "             "
+i_r = int(i_r)  #transforma os valores em numeros inteiro, para não dar erro no loop
+j_r = int(j_r)
+#condições iniciais para os loops
 i = 0
 j = 0
-for i in range(i_r)
- 
+#definir matriz
+m = np.eye(i_r,j_r) #cria uma matriz identidade com i_r linhas e j_r colunas
 
+'''print(i_r)
+print(j_r)'''
+
+for i in range(i_r):
+
+    for j in range(j_r):
+        m[i,j] = 0.1+((1+np.sin((2*x)+(3*y)))/(3.5+np.sin(x-y)))
+        y += y_p
+    j = 0
+    x += x_p
+
+print(m)
