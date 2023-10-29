82ede33b58af435138bfb565c3b0632fff83a890
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2023-10-29 22:21:26 +0000

    final final

--- ex2.py
+++ ex2.py
@@ -7,19 +7,19 @@ import math
 #definir valores iniciais de x e y:
 x_0 = -2 
 y_0 = -3  
-#definir passo
+#definir passo:
 x_p = 0.1 
 y_p = 0.2
-#definir "range" (i_r linhas e j_r colunas)
+#definir "range" (i_r linhas e j_r colunas):
 i_r = (abs(x_0)*2)/x_p    #assumindo que o intervalo é sempre [-a,a]
 j_r = (abs(y_0)*2)/y_p    #   "             "
-i_r = int(i_r)  #transforma os valores em numeros inteiro, para não dar erro no loop
+i_r = int(i_r)  #transforma os valores em numeros inteiros, para não dar erro no loop
 j_r = int(j_r)
 
-#definir matriz
+#definir matriz:
 m = np.eye(i_r,j_r) #cria uma matriz identidade com i_r linhas e j_r colunas
 
-#condições iniciais para os loops
+#condições iniciais para os loops:
 i = 0
 j = 0
 x = x_0
@@ -27,6 +27,7 @@ y = y_0
 #print(i_r)
 #print(j_r)
 
+#loop para atribuir os valores à matriz:
 for i in range(i_r):
 
     for j in range(j_r):
@@ -36,22 +37,26 @@ for i in range(i_r):
     y = y_0
     x += x_p
 
+#escrever a matriz num ficheiro de texto:
+
 '''with open("Matriz.txt", "a") as file:
-    np.savetxt(file, m, delimiter="\t", fmt="%f") ''' #para acrescentar os valores da matriz ao ficheiro
+    np.savetxt(file, m, delimiter="\t", fmt="%f") ''' #para acrescentar os valores da matriz ao ficheiro, não é necessário neste caso
+
 np.savetxt("Matriz.txt", m, delimiter="\t", fmt="%f") #dá overwrite ao ficheiro
 print(m)
 
 
 #Exercício 2:
 
-#reset das condições iniciais
+#reset das condições iniciais:
 i = 0
 j = 0
-#definir uma variável para ser o menor numero e atribuir um valor inicial
+#definir uma variável para ser o menor numero e atribuir um valor inicial:
 a = m[0,6] 
-#definir uma variável para ser as "coordenadas" desse numero
+#definir uma variável para ser as "coordenadas" desse numero:
 a_c =np.array([0,0])
 
+#loop que dá "scan" aos elementos da matriz (6 coluna) e deteta qual o menor:
 for i in range(i_r):
     if m[i,6] <= a:
         a = m[i,6]
@@ -69,11 +74,12 @@ print("2.O menor elemento da sexta coluna é:",a_c,"com o valor:",a)
 #reset das condições iniciais
 i = 0
 j = 0
-#definir uma variável para ser o menor numero e atribuir um valor inicial
+#definir uma variável para ser o menor numero e atribuir um valor inicial:
 b = m[0,0] 
-#definir uma variável para ser as "coordenadas" desse numero
+#definir uma variável para ser as "coordenadas" desse numero:
 b_c =np.array([0,0])
 
+#loop que dá "scan" aos elementos da matriz e deteta qual o menor:
 for i in range(i_r):
 
     for j in range(j_r):
@@ -91,14 +97,15 @@ print("3.O menor elemento da Matriz é:",b_c,"com o valor:",b)
 
 #Exercício 4:
 
-#reset das condições iniciais
+#reset das condições iniciais:
 i = 0
 j = 0
-#definir uma variável para ser o maior numero e atribuir um valor inicial
+#definir uma variável para ser o maior numero e atribuir um valor inicial:
 c = m[0,0] 
-#definir uma variável para ser as "coordenadas" desse numero
+#definir uma variável para ser as "coordenadas" desse numero:
 c_c =np.array([0,0])
 
+#loop que dá "scan" aos elementos da matriz e deteta qual o maior:
 for i in range(i_r):
 
     for j in range(j_r):
