242e369cb043a4d664fedb72b878a9c3647fab88
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2023-10-29 16:39:08 +0000

    update

--- ex2.py
+++ ex2.py
@@ -4,14 +4,14 @@ import math
 #Exercício 1
 
 #definir valores iniciais de x e y:
-x = -2
-y = -3
+x = -2 
+y = -3  
 #definir passo
 x_p = 0.1 
 y_p = 0.2
 #definir "range"
-i_r = (abs(x)*2)/x_p
-j_r = (abs(y)*2)/y_p
+i_r = (abs(x)*2)/x_p    #assumindo que o intervalo é sempre [-a,a]
+j_r = (abs(y)*2)/y_p    #   "             "
 i = 0
 j = 0
 for i in range(i_r)
