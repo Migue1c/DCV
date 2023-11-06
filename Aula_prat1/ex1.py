7f7631430cbe0283df7a3c10075ee4430686e0f5
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2023-11-06 00:45:33 +0000

    1

---
+++ Aula_prat1/ex1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import math
+
+#definir o vetor original:
+Cg_P = np.array([[120],
+                [130],
+                [140]])
+
+#definir valores para os ângulos:
+alfa = math.radians(5)
+beta = math.radians(1)
+teta = math.radians(60)
+phi = math.radians(30)
+psi = math.radians(45)
+
+#definir matriz de transformação:
+
