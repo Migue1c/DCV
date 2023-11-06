9f240316ce95a747a860dd10d29e6bdb79daebf0
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2023-11-06 15:57:57 +0000

    ex1 v2

--- Aula_prat1/ex1.py
+++ Aula_prat1/ex1.py
@@ -15,7 +15,10 @@ psi = math.radians(45)
 
 #definir matriz de transformação (como uma função):
 def T(a,b): #a->alpha e b->beta
-    return np.array([[math.cos(a)*math.sin(b), -math.cos(a)*math.sin(b), -math.sin(a)],  
+    return np.array([[math.cos(a)*math.cos(b), -math.cos(a)*math.sin(b), -math.sin(a)],  
                      [math.sin(b), math.cos(b), 0],
                      [math.sin(a)*math.cos(b), -math.sin(a)*math.sin(b), math.cos(a)]])
 
+#resultado:
+Cg_P_aeronave = T(alpha,beta).dot(Cg_P)
+print("Cg_P(Rb):",Cg_P_aeronave[0],"Xb",Cg_P_aeronave[1],"Yb",Cg_P_aeronave[2],"Zb")
