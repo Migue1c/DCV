5a754f72d7f14acc5a3a3401a52fb16eb29107bf
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2023-11-08 22:26:12 +0000

    ex3

---
+++ Aula_prat1/ex3.py
@@ -0,0 +1,18 @@
+import numpy as np
+import math
+
+#Exercício 2
+
+#Dados do enunciado:
+
+theta_0 = 89.9
+phi_0 = 30
+psi_0 = 45
+p = 50
+q = 30
+r = 103
+h = 0.01
+
+#definir vetores x_0 e w
+x_0 = np.array([phi_0, theta_0, psi_0])
+w = np.array([p, q, r])
