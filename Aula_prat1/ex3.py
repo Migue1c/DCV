a50c4fe373e3282d9d93a71ef8ea2b7f72dfa28d
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2023-11-08 22:41:19 +0000

    ta bom por hoje

--- Aula_prat1/ex3.py
+++ Aula_prat1/ex3.py
@@ -16,3 +16,11 @@ h = 0.01
 #definir vetores x_0 e w
 x_0 = np.array([phi_0, theta_0, psi_0])
 w = np.array([p, q, r])
+
+def eta_v0(x)
+    
+    eta_0 = np.cos(x[0]/2)*np.cos(x[1]/2)*np.cos(x[2]/2)+np.sin(x[0]/2)*np.sin(x[1]/2)*np.sin(x[2]/2)
+    eta_1 = np.sin(x[0]/2)*np.cos(x[1]/2)*np.cos(x[2]/2)-np.cos(x[0]/2)*np.sin(x[1]/2)*np.sin(x[2]/2)
+    eta_2 = np.cos(x[0]/2)*np.sin(x[1]/2)*np.cos(x[2]/2)-np.sin(x[0]/2)*np.cos(x[1]/2)*np.sin(x[2]/2)
+    eta_3 = np.cos(x[0]/2)*np.cos(x[1]/2)*np.sin(x[2]/2)-np.sin(x[0]/2)*np.sin(x[1]/2)*np.cos(x[2]/2)
+
