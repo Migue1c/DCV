aa80cb58d3daa05793378e4693c2cf07d71cd1e4
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2024-01-09 14:07:36 +0000

    rename

--- Mini-prrrojeto/tarefa 1.py
+++ Mini-prrrojeto/tarefa 1.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 # Reading the Sheet
 df_read     = ['tempo','p','q','r']
-df_pqr         = pd.read_excel('AnexoA.xls', sheet_name = 'Sheet14', usecols = df_read, skiprows = 5)
+df_pqr         = pd.read_excel('Anexo_A.xls', sheet_name = 'Sheet14', usecols = df_read, skiprows = 5)
 #print(df_pqr) 
     
 df_read1 = ['teta0', 'fi0', 'psi0']
-df_ang = pd.read_excel('AnexoA.xls', sheet_name = 'Sheet14', usecols = df_read1, skiprows = 1, nrows=1)
+df_ang = pd.read_excel('Anexo_A.xls', sheet_name = 'Sheet14', usecols = df_read1, skiprows = 1, nrows=1)
 #print(df_ang)
