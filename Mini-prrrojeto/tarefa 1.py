41bd650590baca8a6fee744051577dd4ffc1004b
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2024-01-10 20:49:27 +0000

    tarefa 1 done

--- Mini-prrrojeto/tarefa 1.py
+++ Mini-prrrojeto/tarefa 1.py
@@ -10,9 +10,9 @@ def eta_v0(x):
     #a = np.radians(x[0])    #phi
     #b= np.radians(x[1])    #theta
     #c = np.radians(x[2])    #psi
-    a = x[0]
-    b = x[1]
-    c = x[2]
+    a = x[0] #phi
+    b = x[1] #theta
+    c = x[2] #psi
 
     eta_0 = np.cos(a/2)*np.cos(b/2)*np.cos(c/2)+np.sin(a/2)*np.sin(b/2)*np.sin(c/2)
     eta_1 = np.sin(a/2)*np.cos(b/2)*np.cos(c/2)-np.cos(a/2)*np.sin(b/2)*np.sin(c/2)
@@ -29,9 +29,9 @@ def eta_v0(x):
 #function to compute M matrix:
 def M(pqr):
     
-    p =  np.radians(pqr[0])
-    q =  np.radians(pqr[1])   #rad/s
-    r =  np.radians(pqr[2])
+    p =  pqr[0]
+    q =  pqr[1]  #rad/s
+    r =  pqr[2]
     
     m = 0.5*np.array([[0, -p, -q, -r],
                      [p, 0, r, -q],
@@ -43,9 +43,9 @@ def M(pqr):
 #function to convert eta_dot values back to euler angles:
 def X_e(eta_u):
     eta = np.array([eta_u[0,0], eta_u[1,0], eta_u[2,0], eta_u[3,0]])
-    phi = np.arctan2(2*(eta[0]*eta[1]+eta[2]*eta[3]), 1-2*((eta[1]**2)+(eta[2]**2)))
+    phi = np.arctan2(2*(eta[0]*eta[1]+eta[2]*eta[3]), 1-(2*((eta[1]**2)+(eta[2]**2))))
     theta = np.arcsin(2*(eta[0]*eta[2]-eta[3]*eta[1]))
-    psi = np.arctan2(2*(eta[0]*eta[3]+eta[1]*eta[2]), 1-2*((eta[2]**2)+(eta[3]**2)) )
+    psi = np.arctan2(2*(eta[0]*eta[3]+eta[1]*eta[2]), 1-(2*((eta[2]**2)+(eta[3]**2))))
 
     eta_1 = np.array([phi, theta, psi])
 
@@ -53,9 +53,10 @@ def X_e(eta_u):
 
 
 
-# Reading the Sheet
+#File reader
 dataf_read     = ['tempo','p','q','r']
-dataf_pqr         = pd.read_excel('AnexoA.xls', sheet_name = 'Sheet14', usecols = dataf_read, skiprows = 5)  #mudar variaveis
+dataf_pqr         = pd.read_excel('AnexoA.xls', sheet_name = 'Sheet14', usecols = dataf_read, skiprows = 5)
+
 #print(dataf_pqr) 
     
 dataf_read1 = ['fi0', 'teta0', 'psi0'] #trocar a ordem das colunas no excel
@@ -64,15 +65,17 @@ dataf_ang = pd.read_excel('AnexoA.xls', sheet_name = 'Sheet14', usecols = dataf_
 
 ang_0 = np.array(dataf_ang) 
 ang_0 = ang_0.reshape(-1)
-#print(ang_0)
+print(ang_0)
 valores_t = np.array(dataf_pqr) # t ; p ; q ; r
 #print(valores_t)
 
 h = valores_t[1,0] - valores_t[0,0]
 #print(h)
 
-ang_i = ang_0
+#matrix to store values
 x_r = np.zeros((len(valores_t),4))
+#initialization
+ang_i = ang_0
 i = 0
 while i < len(valores_t):
     #store values
@@ -80,18 +83,19 @@ while i < len(valores_t):
     x_r[i,1] = ang_i[0]
     x_r[i,2] = ang_i[1]
     x_r[i,3] = ang_i[2]
-    #compute next iteration
+    #compute current iteration
     eta_i = eta_v0(ang_i)
     pqr = np.array([valores_t[i,1], valores_t[i,2], valores_t[i,3]])
     m_i = M(pqr)
     m_d = sp.linalg.expm(m_i * h)
     eta_1 = m_d @ eta_i
-    ang_i = X_e(eta_1)
-    #increase i value
+    print(eta_1)
+    #values for next iteration
+    ang_i = X_e(eta_1) #value will be stored on the next iteration
     i += 1
 #print(x_r)
 
-#convert to dataframe (easier plotting)
+#convert matrix to dataframe (easier plotting)
 x_dataf = pd.DataFrame(x_r, columns = ['tempo', 'phi', 'theta', 'psi'])
 #print(x_dataf)
 
